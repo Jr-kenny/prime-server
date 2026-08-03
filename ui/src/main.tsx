59639d42e08d2ec061ac77f7ee921cea309a8d3d
jr-kenny <dalvidjr2022@gmail.com>
2026-08-04 00:07:29 +0100

    Change Prime Server UI accents to blue

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./App";
 import "./styles.css";
 import "./light.css";
 import "./actual.css";
+import "./blue.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode><App /></React.StrictMode>
